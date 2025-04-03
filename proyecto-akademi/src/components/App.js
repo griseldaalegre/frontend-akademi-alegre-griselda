9de6effab143913f73d4e43e6c83e56765ca9623
griseldaalegre <alegregriselda5@gmail.com>
2025-04-03 09:57:38 -0300

    feat: formulario de alta, eliminación con Redux y ruteo en la app.

--- proyecto-akademi/src/components/App.js
+++ proyecto-akademi/src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ProductList from "./ProductList";
+import ProductCreate from "./ProductCreate";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="ui ">
-      <ProductList/>
+    <div className="ui container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add-product" element={<ProductCreate />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
